271afaed1aaf753b00d5cc74ebd541e638701fa8
ayush <ayush@AYUSHGANDHI>
2024-01-29 11:16:10 +0530

    Add project files.

---
+++ weekend weekdays switch case/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weekend_weekdays_switch_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of the day:");
+            string dayName = Console.ReadLine();
+
+            switch (dayName)
+            {
+                case "monday":
+                case "tuesday":
+                case "wednesday":
+                case "thursday":
+                case "friday":
+                    Console.WriteLine("Weekday");
+                    Console.ReadKey();
+                    break;
+                case "saturday":
+                case "sunday":
+                    Console.WriteLine("Weekend");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Invalid input ");
+                    Console.ReadKey();
+                    break;
+                 
+
+            }
+        }
+    }
+}
